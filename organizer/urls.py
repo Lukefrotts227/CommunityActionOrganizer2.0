030183a16dd8e3798e116a8c3d260eefc5831583
Ljf227 <ljfrotton@gmail.com>
2023-11-03 09:12:26 -0400

    cleared and ready to get to the planning

--- organizer/urls.py
+++ organizer/urls.py
@@ -4,6 +4,4 @@ from .views import index, api_test, route_test
 
 urlpatterns = [
     path('', index, name='index'),
-    path('api/test', api_test, name='api_test'),
-    path('route/test', route_test, name='route_test')
 ]
--- organizer/views.py
+++ organizer/views.py
@@ -5,15 +5,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    if request.htmx:
-        # This is an htmx request, return a partial
-        return HttpResponse('<p>This is an htmx response!</p>')
-    else:
-        # This is a full page request
-        return render(request, 'index.html')
+    # This is a full page request
+    return render(request, 'index.html')
 
-def api_test(request): # This is
-    return HttpResponse('<p>This is an htmx response!</p>')
 
-def route_test(request):
-    return render(request, 'route.html')
